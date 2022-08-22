81b901a765e144792fad580ef0f29426f2119476
Santiago Rambeaud <santy_r_7@hotmail.com>
2022-08-21 23:08:27 -0300

    ParteDelFrontFinish

---
+++ client/src/store/index.js
@@ -0,0 +1,6 @@
+import { createStore, applyMiddlware} from 'redux';
+import { composeWithDevTools } from  'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducer';
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddlware(thunk)));
